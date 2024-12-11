11b23ba1ce2abe60e27bcfb8f1a2c1522c64533c
thaingan-ueh <nganvo.31221025581@st.ueh.edu.vn>
2024-12-11 21:05:12 +0700

    header

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="header.css">
+</head>
+<body>
+    <header>
+            <img src="./Ảnh/Logo.png">
+        <div class="header-left">
+            <a href="#"><strong>TRANG CHỦ</strong></a>
+            <div class="drop-down">
+            <a href="#"><strong>SẢN PHẨM</strong></a>
+                <div class="detail">
+                    <a href="danhmucanimals.html"><strong>ANIMALS</strong></a>
+                    <a href="danhmucbags&charms.html"><strong>BAGS & CHARMS</strong></a>
+                    <a href="danhmucamuseables.html"><strong>AMUSEABLES</strong></a>
+                    <a href="danhmucgifts.html"><strong>GIFTS</strong></a>
+                </div>
+            </div>
+            <a href="#"><strong>TIN TỨC</strong></a>
+        </div>
+        <div class="header-right">
+            <div class="search-bar">
+                <form id="searchForm">
+                    <input type="text" id="searchInput" placeholder="Nhập từ khóa...">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                  </form>
+                <a href="#"><i class="fa fa-shopping-cart"></i></a>
+                <a href="#"><i class="fa fa-user"></i></a>
+            </div>
+        </div>
+    </header>
